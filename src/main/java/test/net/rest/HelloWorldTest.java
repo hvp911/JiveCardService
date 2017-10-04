5977711383a73f5f443267f936de96602d4cb364
hardik.patel <hardik.patel@bhvr.com>
2017-10-04 16:53:27 -0400

    Adding sample test case

---
+++ src/main/java/test/net/rest/HelloWorldTest.java
@@ -0,0 +1,12 @@
+package test.net.rest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HelloWorldTest {
+   @Test
+   public void test1a(){
+      assertTrue(true);
+   }
+}
